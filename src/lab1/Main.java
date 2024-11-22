75ad2f9ec06241ebef05cab2aeb50ca8915cba8b
Александр Маркелов <114938226+Aleksandr1209@users.noreply.github.com>
2024-11-22 15:43:13 +0400

    Update Main.java

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,46 +1,32 @@
-package lab1;
-
-import lab1.classes.*;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
+
+import classes.FileGenerator;
+import classes.ParallelProcessor;
+import classes.SequentialProcessor;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 1000000;
-        int multiplier = 2;
-        int numThreads = 4;
-        String inputFilename = "numbers.txt";
-        String outputFilename = "processed_numbers.txt";
-
-        FileGenerator.generateFile(N, inputFilename);
-        System.out.println("Файл с числами сгенерирован.");
-
-        long startTime = System.nanoTime();
-        SequentialProcessor.processFile(inputFilename, outputFilename, multiplier);
-        long endTime = System.nanoTime();
-        System.out.println("Время последовательной обработки: " + (endTime - startTime) + " нс");
-
-        int[] array = new int[N];
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilename))) {
-            String line;
-            int index = 0;
-            while ((line = reader.readLine()) != null) {
-                array[index++] = Integer.parseInt(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String inputfile = "numbers.txt";
+        String outputfile = "output.txt";
+        String configName = "app.config";
+        Properties prop = new Properties();
+        try (FileInputStream fis = new FileInputStream(configName)) {
+            prop.load(fis);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-
-        PerformanceAnalyzer.measurePerformance(array, multiplier, numThreads);
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ComplexOperations.square(array[i]);
-        }
-        System.out.println("Сложная операция выполнена.");
-
-        ParallelProcessor.processArray(array, multiplier, 2);
-        System.out.println("Неравномерная загрузка потоков выполнена.");
+        int N = Integer.parseInt(prop.getProperty("N"));
+        int threadsCount = Integer.parseInt(prop.getProperty("THREADS_COUNT"));
+        int operation = Integer.parseInt(prop.getProperty("OPERATION"));
+        FileGenerator.generateFile(N, "numbers.txt");
+        SequentialProcessor.processFile(inputfile, outputfile);
+        ParallelProcessor.processArray(inputfile, N, threadsCount, outputfile);
+        //SequentialProcessor.processFile(inputfile, outputfile, operation);
+        //ParallelProcessor.processArray(inputfile, N, threadsCount, outputfile, operation);
     }
 }
